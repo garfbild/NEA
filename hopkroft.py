fb4ca03e67bf23458339c775cd2708bb84eba4ad
garfbild <30866252+garfbild@users.noreply.github.com>
2020-02-20 11:40:24 +0000

    Update hopkroft.py

--- hopkroft.py
+++ hopkroft.py
@@ -19,7 +19,6 @@ rawgraph = np.zeros((width+1,height+1),dtype=object)
 M = []
 for i in range(width):
     M.append([0,0])
-print(M)
 #u    v
 #u1    v3
 #u2    v2
@@ -31,13 +30,14 @@ for i in range(1,width+1):
     for j in range(random.randint(3,4)):
         rawgraph[i][random.randint(1,width)] = 1
 
+#adding labels around the edge
 for u in range(width):
     rawgraph[0][u+1] = u+1
 for v in range(height):
     rawgraph[v+1][0] = v+1
 graph = copy.deepcopy(rawgraph)
-print(graph)
 
+#bfs to get initial matching
 for u in range(1,height+1):
     M[u-1][0] = u
     for v in range(1,width+1):
@@ -47,7 +47,8 @@ for u in range(1,height+1):
                 graph[u][p] = 0
                 graph[p][v] = 0
             break
-#print(M)
+
+#finding free freevertices
 graph = copy.deepcopy(rawgraph)
 freevertices = []
 for node in range(width):
@@ -64,9 +65,7 @@ while i < len(freevertices):
         freevertices[i] = "v{}".format(freevertices[i])
         i+=1
 
-print("freevertices",freevertices)
-print(M)
-
+#converting adjacency matrix into adjacency list
 adjgraph = {}
 for u in range(1,height+1):
     temp = []
@@ -81,13 +80,11 @@ for v in range(1,width+1):
         if graph[u][v] == 1:
             temp.append("u{}".format(u))
     adjgraph["v{}".format(v)] = temp
-
-#print(adjgraph)
+print(freevertices)
+print(M)
 
 def DepthFirstSearch(visited,node,graph):
-    if visited != []:
-        if visited[-1][0] == "u" and M[int(visited[-1][1:])-1][1] == 0:
-            return visited
+    print(node)
     if node not in visited:
         visited.append(node)
         #print(node)
@@ -99,20 +96,13 @@ def DepthFirstSearch(visited,node,graph):
                 DepthFirstSearch(visited,n,graph)
     return visited
 
-
+#for each free vertex we find the augmenting path between two free vertices
 for freevertex in freevertices:
     visited = []
     augmentingpath = DepthFirstSearch(visited,freevertex,adjgraph)
+    print(augmentingpath)
     if augmentingpath[-1][0] == "u" and M[int(augmentingpath[-1][1:])-1][1] == 0:
+        #symmetric difference between path and matching
         for node in range(1,len(augmentingpath)):
             if augmentingpath[node][0] == "u" and augmentingpath[node-1][0] == "v":
                 M[int(augmentingpath[node][1:])-1][1] = int(augmentingpath[node-1][1:])
-    print("testing")
-    for match in M:
-        if graph[match[0]][match[1]] == 1:
-            print("True")
-        else:
-            print(match)
-    print(freevertex,"augmentingpath",augmentingpath)
-    print(M)
-print(adjgraph)
