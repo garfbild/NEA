696ac4198f72024725aa458a2933e46b2f8e4afe
garfbild <30866252+garfbild@users.noreply.github.com>
2020-02-26 16:32:57 +0000

    Add files via upload

--- hopkroft.py
+++ hopkroft.py
@@ -1,11 +1,16 @@
-import numpy as np
 import random
 import string
 import copy
 
-height,width = 10,10
 
-rawgraph = np.zeros((width+1,height+1),dtype=object)
+rawgraph = []
+
+height,width = 10,10
+for x in range(width+1):
+    rawgraph.append([])
+    for y in range(height+1):
+        rawgraph[x].append(0)
+    
 #1----1
 #2-\  2
 #3  \-3
@@ -18,7 +23,7 @@ rawgraph = np.zeros((width+1,height+1),dtype=object)
 
 M = []
 for i in range(width):
-    M.append([0,0])
+    M.append(0)
 #u    v
 #u1    v3
 #u2    v2
@@ -37,12 +42,12 @@ for v in range(height):
     rawgraph[v+1][0] = v+1
 graph = copy.deepcopy(rawgraph)
 
+
 #bfs to get initial matching
 for u in range(1,height+1):
-    M[u-1][0] = u
     for v in range(1,width+1):
         if graph[u][v] == 1:
-            M[u-1][1] = v
+            M[u-1] = v
             for p in range(1,width+1):
                 graph[u][p] = 0
                 graph[p][v] = 0
@@ -54,8 +59,8 @@ freevertices = []
 for node in range(width):
     freevertices.append(node+1)
 for m in M:
-    if m[1] != 0:
-        freevertices[m[1]-1] = "#"
+    if m != 0:
+        freevertices[m-1] = "#"
 
 i = 0
 while i < len(freevertices):
@@ -80,29 +85,54 @@ for v in range(1,width+1):
         if graph[u][v] == 1:
             temp.append("u{}".format(u))
     adjgraph["v{}".format(v)] = temp
-print(freevertices)
-print(M)
 
+global path
+visited = []
+path = []
+#depth first search.
 def DepthFirstSearch(visited,node,graph):
-    print(node)
+    #we pass the path as we back up through the recursion
+    global path
+    if visited != []:
+        if visited[-1][0] == "u" and M[int(visited[-1][1:])-1] == 0:
+            path.append(node)
+            return visited
     if node not in visited:
         visited.append(node)
-        #print(node)
         for n in graph[node]:
+            if visited != []:
+                if visited[-1][0] == "u" and M[int(visited[-1][1:])-1] == 0:
+                    path.append(node)
+                    return visited
             if node[0] == "u":
-                if int(n[1:]) == M[int(node[1])-1][1]:
-                    DepthFirstSearch(visited,n,graph)
+                if int(n[1:]) == M[int(node[1])-1]:
+                    visited = DepthFirstSearch(visited,n,graph)
             else:
-                DepthFirstSearch(visited,n,graph)
+                visited = DepthFirstSearch(visited,n,graph)
+    if visited != []:
+        if visited[-1][0] == "u" and M[int(visited[-1][1:])-1] == 0:
+            path.append(node)
+            return visited
+
     return visited
 
-#for each free vertex we find the augmenting path between two free vertices
+print(freevertices)
 for freevertex in freevertices:
-    visited = []
-    augmentingpath = DepthFirstSearch(visited,freevertex,adjgraph)
-    print(augmentingpath)
-    if augmentingpath[-1][0] == "u" and M[int(augmentingpath[-1][1:])-1][1] == 0:
-        #symmetric difference between path and matching
-        for node in range(1,len(augmentingpath)):
-            if augmentingpath[node][0] == "u" and augmentingpath[node-1][0] == "v":
-                M[int(augmentingpath[node][1:])-1][1] = int(augmentingpath[node-1][1:])
+    path = []
+    DepthFirstSearch(visited,freevertex,adjgraph)
+    path.reverse()
+    print(path)
+    if path != []:
+        if M[int(path[-1][1:])-1] == 0:
+            #symmetric difference of a path and matching
+            for x in range(len(path)):
+                if path[x][0] == "u":
+                    M[int(path[x][1:])-1] = int(path[x-1][1:])
+        else:
+            print(freevertex,"has no augmentingpath")
+    else:
+        print(freevertex,"has no augmentingpath")
+
+print(M)
+    
+    
